8ba49e7dddc5057d7d87cef34479b3cc19d566d0
Vismaya <vadanavismaya@gmail.com>
2024-06-29 13:00:18 +0530

    Day 23: Made the password an environment variable for security

--- send_email.py
+++ send_email.py
@@ -1,11 +1,11 @@
-import smtplib, ssl
+import smtplib, ssl, os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "vadanavismaya@gmail.com"
-    password = "xugjnimpjwudgozh"
+    password = os.getenv("PASSWORD")
     receiver = "vadanavismaya@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
